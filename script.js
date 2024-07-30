a4b107d219d9cd09b03bfce9fe767bf8af5bfe0a
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-07-30 19:56:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your code here
+let container = document.querySelector('main');
+
+let imageClassName = ["img1", "img2", "img3", "img4", "img5"]
+
+let randomIndex = parseInt(Math.random()*5);
+let randomClassName = imageClassName[randomIndex];
+imageClassName.push(randomClassName);
+
+let count = 0;
+for (let i = 0; count < 6; i++) {
+	let imgTag = document.createElement('img');
+	let randomIndex = parseInt(Math.random()*6);
+	if (imageClassName[randomIndex] == 1) {
+		continue;
+	}
+	imgTag.className = imageClassName(randomIndex);
+	count++;
+	imageClassName[randomIndex] = -1;
+	container.append(imgTag);
+}
